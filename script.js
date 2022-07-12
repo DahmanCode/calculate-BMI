16ffc578982738ef29866f0e198e9033d782d606
DahmanCode <elaammariabderrahman@gmail.com>
2022-07-12 13:51:36 +0100

    change color fo category

--- script.js
+++ script.js
@@ -11,23 +11,29 @@ function calculate() {
   bmi = (weight / Math.pow((height / 100), 2)).toFixed(1);
   result.textContent = bmi;
 
+  cg = document.getElementById("category");
+
   if (bmi < 18.5) {
-    category = "Underweight";
+    category = "Underweight"
     result.style.color = "#ffc44d";
+    cg.style.color = "#ffc44d";
   }
   else if (bmi >= 18.5 && bmi <= 24.9) {
     category = "Normal Weight"
     result.style.color = "#0be881";
+    cg.style.color = "#0be881";
   }
   else if (bmi >= 25 && bmi <= 29.9) {
     category = "Overweight"
     result.style.color = "#ff884d";
+    cg.style.color = "#ff884d";
   }
   else {
     category = "Obesity"
     result.style.color = "#ff5e57";
+    cg.style.color = "#ff5e57";
   }
 
-  document.getElementById("category").textContent = category;
+  cg.textContent = category;
 
 }
